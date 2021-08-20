73c38706364c166eeaeb370c18f25aa8302c1b53
md.asif.8393 <md.asif.8393@gmail.com>
2021-08-20 21:26:29 +0600

    practise

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+    import logo from './logo.svg';
+    import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    function App() {
+      const nayoks = ['Rajjak', 'Anomgir', 'Manna', 'Joshim', ];
+      const products = [
+        {name: 'Ball', price:'$120'},
+          {name: 'Pen', price: '$20'},
+          {name: 'Mouse', price: '$200'},
+      ]
 
-export default App;
+      
+      return (
+        <div className="App">
+          <header className="App-header">
+            <ul>
+              {
+                nayoks.map(nayok => <li>{nayok}</li>)
+              }
+              {
+                products.map(product => <li>{product.name}</li>)
+              }
+            </ul>
+              {
+                products.map(p => <Product product={p}> </Product>)
+              }
+          </header>
+        </div>
+      );
+    }
+
+    
+
+    function Product(props){
+      const {name, price} = props.product;
+
+      const style1 = {
+        backgroundColor: 'gray',
+        height: '200px',
+        width: '200px',
+        borderRadius: '10px',
+        float: 'left',
+        margin: '10px',
+      }
+      return(
+        <div style={style1}>
+          <h3>Name: {name}</h3>
+          <h2>Price: {price}</h2>
+          <button>Bye Now</button>
+        </div>
+      )
+    }
+
+    export default App;
