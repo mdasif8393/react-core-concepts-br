ce340b3e1a12d55a1abbf474d73871d8a40efe29
md.asif.8393 <md.asif.8393@gmail.com>
2021-08-21 23:57:00 +0600

    complete

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
     import logo from './logo.svg';
     import './App.css';
+import { useState } from 'react/cjs/react.development';
+import { useEffect } from 'react';
 
     function App() {
       const nayoks = ['Rajjak', 'Anomgir', 'Manna', 'Joshim', ];
       const products = [
-        {name: 'Ball', price:'$120'},
+          {name: 'Ball', price:'$120'},
           {name: 'Pen', price: '$20'},
           {name: 'Mouse', price: '$200'},
       ]
-
       
       return (
         <div className="App">
           <header className="App-header">
+          <Counter></Counter>
+          <Users> </Users>
             <ul>
               {
                 nayoks.map(nayok => <li>{nayok}</li>)
@@ -24,9 +27,42 @@
               {
                 products.map(p => <Product product={p}> </Product>)
               }
+             
           </header>
         </div>
       );
+    }
+
+    function Users () {
+      const [users, setUsers] = useState([]);
+
+      useEffect(()=>{
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then(data => setUsers(data))
+      },[])
+
+      return(
+        <div>
+          <ul>
+            {
+              users.map(user => <li>{user.name}</li>)
+            }
+          </ul>
+        </div>
+      )
+    }
+
+    function Counter (){
+      const [count, setCount] = useState(10);
+      const handleIncrease = () => setCount(count + 1);
+      return(
+        <div>
+          <h1>Counter: {count}</h1>
+          <button onClick = { () => setCount(count - 1)}>Decrease</button>
+          <button onClick={ () => setCount(count + 1)}>Increase</button>
+        </div>
+      )
     }
 
     
@@ -50,5 +86,7 @@
         </div>
       )
     }
+
+    
 
     export default App;
